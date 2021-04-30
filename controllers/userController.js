832e54646a5f683870ff6933013209eeae212b6a
Gregory Bertrand <gregoryalexbertrand@gmail.com>
2021-04-30 10:45:18 -0500

    user encrypt & password hash

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,22 @@
 const models = require('../models')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userController = {}
 
 userController.create = async (req, res) => {
     try {
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10)
         let user = await models.user.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: hashedPassword
         })
-        res.json({user})
+        const encryptedId = jwt.sign({userId: user.id}, process.env.JWT_SECRET)
+        console.log(encryptedId)
+        res.json({message: 'success', userId: encryptedId, user})
     } catch (error) {
+        console.log(error)
         res.json({error})
     }
 }
@@ -20,8 +26,9 @@ userController.login = async (req, res) => {
         const user = await models.user.findOne({
             where: {email: req.body.email}
         })
-        if (user.password === req.body.password) {
-            res.json({user, message: 'welcome'})
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+            const encryptedId = jwt.sign({userId: user.id}, process.env.JWT_SECRET)
+            res.json({ message: 'welcome', userId: encryptedId, user})
         } else {
             res.status(401).json({message: 'attempt failed'})
         }
